59c9c1a3115971e08a76e81ddfbe0587a9cd5b5b
Saloiva <oiva.salomaa@gmail.com>
2024-10-21 12:48:28 +0300

    lisatty kayttajan syote

--- Lucky_7/src/App.java
+++ Lucky_7/src/App.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+
+        // Kysytaan, kuinka paljon kayttaja haluaa ladata saldoa
+        System.out.println("How much money will you use?");
+        int saldo = scanner.nextInt();
+
+        // Maaritellaan pyoraytyksen hinta
+        int hinta = 1;
+
+        System.out.println("One spin costs one token.");
+
+        // Kysytaan, haluaako kayttaja pelata
+        scanner.nextLine(); // Tyhjennetään syötteen puskuri
+
+        scanner.close();
     }
 }
