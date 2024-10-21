7c96b48db8803efae8b91dfe327f80585d93cc3d
Saloiva <oiva.salomaa@gmail.com>
2024-10-21 12:52:12 +0300

    while-loop lisatty

--- Lucky_7/src/App.java
+++ Lucky_7/src/App.java
@@ -18,6 +18,56 @@ public class App {
         // Kysytaan, haluaako kayttaja pelata
         scanner.nextLine(); // Tyhjennetään syötteen puskuri
 
+        while (saldo >= hinta) {
+            System.out.println("Do you want to play?");
+            System.out.println("If yes, press enter.");
+            System.out.println("If not, type: I don't want to play this game anymore.");
+
+            String vastaus = scanner.nextLine();
+
+            // Jos haluaa pelata
+            if (vastaus.equalsIgnoreCase("")) {
+                saldo -= hinta;
+
+                int randomNum1 = random.nextInt(10);
+                int randomNum2 = random.nextInt(10);
+                int randomNum3 = random.nextInt(10);
+
+                System.out.println(randomNum1);
+                System.out.println(randomNum2);
+                System.out.println(randomNum3);
+
+                // Tarkistetaan tulos
+                if (randomNum1 == 7 && randomNum2 == 7 && randomNum3 == 7) {
+                    System.out.println("All the numbers were 7.");
+                    System.out.println("JACKPOT YOU WON 10 TOKENS!!!");
+                    saldo += 10;
+                } else if ((randomNum1 == 7 && randomNum2 == 7) || (randomNum2 == 7 && randomNum3 == 7) || (randomNum1 == 7 && randomNum3 == 7)) {
+                    System.out.println("Two of the numbers were 7!!");
+                    System.out.println("You won 5 tokens!!");
+                    saldo +=5;
+                } else if (randomNum1 == 7 || randomNum2 == 7 || randomNum3 == 7) {
+                    System.out.println("One of the numbers was 7!");
+                    System.out.println("You won 3 tokens!");
+                    saldo +=3;
+                } else {
+                    System.out.println("You lose.");
+                }
+
+                System.out.println("You have now " + saldo + " tokens left.");
+            }
+            // Jos käyttäjä haluaa lopettaa pelin
+            else if (vastaus.equalsIgnoreCase("I don't want to play this game anymore.")) {
+                System.out.println("You have now " + saldo + " tokens. Thanks for playing!");
+                break;
+            }
+            // Jos syöte on virheellinen
+            else {
+                System.out.println("Yes or no?");
+                continue; // Palataan kysymään uudelleen
+            }
+        }
+        
         scanner.close();
     }
 }
